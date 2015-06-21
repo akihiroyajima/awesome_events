d917af3e0167416cf79ce8c657f49e781a80a4f0
akihiroyajima <akihiroyazima@gmail.com>
2015-06-22 08:25:38 +0900

    update rspec

--- Gemfile
+++ Gemfile
@@ -49,6 +49,11 @@ gem 'carrierwave', '~> 0.10.0'
 
 gem 'mini_magick', '~> 3.7.0'
 
+group :test do
+	gem 'shoulda-matchers', '~> 2.6.0'
+end
+
 group :development, :test do
 	gem 'rspec-rails', '3.0.1'
+	gem 'factory_girl_rails', '~> 4.4.1'
 end
--- spec/models/event_spec.rb
+++ spec/models/event_spec.rb
@@ -1,14 +1,23 @@
 require 'rails_helper'
 
-RSpec.describe Event, :type => :model do
+describe Event do
 	describe '#name' do
-		context '空白のとき' do
-			let(:event) { Event.new(name: '') }
+		it { should validate_presence_of(:name) }
+		it { should ensure_length_of(:name).is_at_most(50) }
+	end
 
-			it 'valid でないこと' do
-				event.valid?
-				expect(event.errors[:name]).to be_present
-			end
+	describe '#created_by?' do
+		let(:event) { create(:event) }
+		subject { event.created_by?(user) }
+
+		context '引数が nil なとき' do
+			let(:user) { nil }
+			it { should be_falsey }
+		end
+
+		context '#owner_id と 引数の #id が同じとき' do
+			let(:user) { double('user', id: event_id) }
+			it { should be_truthy }
 		end
 	end
 end
